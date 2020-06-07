19fac2c607024f1fcf720d3c10b931c0d94c6d46
Andreas Funke <andreas.funke@uni-duesseldorf.de>
2020-06-07 14:14:47 +0200

    removal of infrequent words fixed

--- src/topic_labeling/preprocessing/semantic_diversity.py
+++ src/topic_labeling/preprocessing/semantic_diversity.py
@@ -4,6 +4,7 @@ import json
 import re
 from collections import Counter
 from random import shuffle
+from time import time
 from typing import Iterable, List
 
 import numpy as np
@@ -11,6 +12,7 @@ import pandas as pd
 from gensim.corpora import Dictionary, MmCorpus
 from gensim.matutils import corpus2dense, corpus2csc
 from gensim.models import TfidfModel, LsiModel
+from tqdm import tqdm
 
 from topic_labeling.topic_modeling.lda import split_corpus
 from topic_labeling.utils.constants import (
@@ -18,6 +20,57 @@ from topic_labeling.utils.constants import (
     POS_N, POS_NV, POS_NVA, SEMD_PATH
 )
 from topic_labeling.utils.utils import init_logging, log_args
+
+
+tqdm.pandas()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--dataset', type=str, required=True)
+    parser.add_argument('--version', type=str, required=False, default='default')
+    parser.add_argument('--nb-files', type=int, required=False, default=None)
+    parser.add_argument('--window', type=int, required=False, default=1000)
+    parser.add_argument('--min-word-freq', type=int, required=False, default=50)
+    parser.add_argument('--min-contexts', type=int, required=False, default=40)
+    parser.add_argument('--nb-topics', type=int, required=False, default=300)
+    parser.add_argument('--pos-tags', nargs='*', type=str, required=False)
+
+    parser.add_argument('--tfidf', dest='tfidf', action='store_true', required=False)
+    parser.add_argument('--no-tfidf', dest='tfidf', action='store_false', required=False)
+    parser.set_defaults(tfidf=False)
+
+    parser.add_argument('--make-contexts', dest='make_contexts', action='store_true')
+    parser.add_argument('--load-contexts', dest='make_contexts', action='store_false')
+    parser.set_defaults(make_contexts=True)
+
+    parser.add_argument('--make-corpus', dest='make_corpus', action='store_true')
+    parser.add_argument('--load-corpus', dest='make_corpus', action='store_false')
+    parser.set_defaults(make_corpus=True)
+
+    parser.add_argument('--make-lsi', dest='make_lsi', action='store_true')
+    parser.add_argument('--load-lsi', dest='make_lsi', action='store_false')
+    parser.set_defaults(make_lsi=True)
+
+    args = parser.parse_args()
+
+    args.dataset = DATASETS.get(args.dataset, args.dataset)
+
+    if args.pos_tags is None:
+        if args.version == 'all':
+            args.pos_tags = []
+        elif args.version == 'noun':
+            args.pos_tags = POS_N
+        elif args.version == 'noun-verb':
+            args.pos_tags = POS_NV
+        elif args.version == 'noun-verb-adj':
+            args.pos_tags = POS_NVA
+        else:
+            args.pos_tags = POS_N
+    args.pos_tags = set(args.pos_tags)
+
+    return args
 
 
 def calculate_semantic_diversity(dictionary, corpus, document_vectors, contexts):
@@ -111,23 +164,50 @@ def tfidf_transform(bow_corpus):
     return tfidf_corpus
 
 
-def remove_infrequent_words(contexts, min_freq):
-    print(f"Filtering words with total frequency < {min_freq}")
-    counter = Counter(token for context in contexts for token in context)
-    filtered = [
-        list(filter(lambda x: counter[x] > min_freq, (token for token in context)))
-        for context in contexts
-    ]
-    print(contexts[0])
-    print(filtered[0])
-    return filtered
+def calculate_chunks(df, window):
+    print(f"Calculating chunks")
+    # TODO: this shouldn't take so long
+    t0 = time()
+
+    def _chunk(group):
+        length = len(group)
+        if length <= window:
+            group['chunk'] = 1
+        else:
+            index = np.arange(length)
+            index = index % window == 0
+            index = index.cumsum()
+            group['chunk'] = index
+        return group
+
+    df = df.groupby(HASH, sort=False, as_index=False).progress_apply(_chunk)
+    print(f'time: {time() - t0}')
+    return df
 
 
-def texts2corpus(contexts, stopwords=None, min_contexts=40, filter_above=1, keep_n=200_000):
+def remove_infrequent_words(df, min_freq, min_contexts):
+    print(f"Filtering words with total frequency < {min_freq}.")
+    size = len(df)
+    frequencies = df[TOKEN].value_counts()
+    frequencies = frequencies[frequencies >= min_freq]
+    df = df[df[TOKEN].isin(frequencies.index)]
+    print(f"{size} => {len(df)} - {size - len(df)} words removed.")
+
+    print(f"Filtering words appearing in less than {min_contexts} contexts.")
+    size = len(df)
+    frequencies = df.groupby([HASH, 'chunk'])[TOKEN].unique().explode().value_counts()
+    frequencies = frequencies[frequencies >= min_contexts]
+    df = df[df[TOKEN].isin(frequencies.index)]
+    print(f"{size} => {len(df)} - {size - len(df)} words removed.")
+
+    return df
+
+
+def texts2corpus(contexts, stopwords=None, filter_above=1, keep_n=200_000):
     print(f"Generating bow corpus and dictionary")
 
     dictionary = Dictionary(contexts, prune_at=None)
-    dictionary.filter_extremes(no_below=min_contexts, no_above=filter_above, keep_n=keep_n)
+    dictionary.filter_extremes(no_above=filter_above, keep_n=keep_n)
 
     # filter some noise (e.g. special characters)
     if stopwords:
@@ -139,6 +219,7 @@ def texts2corpus(contexts, stopwords=None, min_contexts=40, filter_above=1, keep
     return bow_corpus, dictionary
 
 
+# TODO: deprecated
 def chunks_from_documents(documents: Iterable, window_size: int) -> List:
     contexts = []
     for document in documents:
@@ -151,7 +232,9 @@ def chunks_from_documents(documents: Iterable, window_size: int) -> List:
     return contexts
 
 
-def make_contexts(dataset, nb_files, pos_tags, window_size=1000):
+def make_contexts(
+        dataset, min_freq=0, min_contexts=0, nb_files=None, pos_tags=None, window_size=1000
+):
     sub_dir = 'dewiki' if dataset.startswith('dewiki') else 'wiki_phrases'
     dir_path = SIMPLE_PATH / sub_dir
 
@@ -166,19 +249,10 @@ def make_contexts(dataset, nb_files, pos_tags, window_size=1000):
 
     if not files:
         print(f"No files found for dataset '{dataset}'")
-        exit()
-
-    files = files[:nb_files]
-    # pattern explained:
-    # - may have leading digits (allows '21_century')
-    # - must have at least one character from the German alphabet (removes '1.')
-    # - must have at least two alphanumeric characters (allows 'A4', but removes 'C.')
-
-    good_ids = None
-    if dataset in GOOD_IDS:
-        good_ids = pd.read_pickle(GOOD_IDS[dataset])
+        raise SystemExit
 
     if nb_files is not None:
+        files = files[:nb_files]
         print(f"Processing {nb_files} files")
 
     nb_words = 0
@@ -188,40 +262,31 @@ def make_contexts(dataset, nb_files, pos_tags, window_size=1000):
 
         print(f"Reading {filename}")
         df = pd.read_pickle(filename)
-        print(f"    initial number of words: {len(df)}")
+        df = df.drop(['sent_idx', 'tok_idx'], axis=1)
 
-        # some datasets have already been filtered, so this may not affect the data
-        if good_ids is not None:
-            df = df[df.hash.isin(good_ids.index)]
-
-        # fixing bad POS tagging
-        mask = df.token.isin(list('[]<>/–%{}'))
-        df.loc[mask, POS] = PUNCT
-
-        # using only certain POS tags
+        # filtering POS tags
         if pos_tags:
+            print(f"    initial number of words: {len(df)}")
+            # fixing wrong POS tagging
+            # TODO: deprecated
+            mask = df.token.isin(list('[]<>/–%{}'))
+            df.loc[mask, POS] = PUNCT
             df = df[df.POS.isin(pos_tags)]
-
+            print(f"    remaining number of words: {len(df)}")
+        df = df.drop(POS, axis=1)
+        # TODO: deprecated - migrate to earlier pipeline
         df[TOKEN] = df[TOKEN].map(lambda x: x.strip('-/'))
 
-        # df = df[df.token.str.len() > 1]
-        # df = df[~df.token.isin(BAD_TOKENS)]
+        df = calculate_chunks(df, window_size)
 
-        # TODO: do we want to remove non-work tokens?
-        df = df[df.token.str.match(WORD_PATTERN)]
+        # TODO: if read from multiple files this needs to be generalized
+        df = remove_infrequent_words(df, min_freq, min_contexts)
         nb_words += len(df)
-        print(f"    remaining number of words: {len(df)}")
 
-        # groupby sorts the documents by hash-id
-        # which is equal to shuffling the dataset before building the model
-        df = df.groupby([HASH], sort=True)[TOKEN].agg(docs_to_lists)
-        documents = df.values.tolist()
-        print(f"    number of documents: {len(documents)}")
-        if window_size > 0:
-            contexts = chunks_from_documents(documents, window_size)
-        else:
-            contexts = documents
-
+        # - shuffling -
+        nb_documents = len(df[HASH].unique())
+        print(f"    number of documents: {nb_documents}")
+        contexts = df.groupby([HASH, 'chunk'], sort=True)[TOKEN].agg(docs_to_lists).to_list()
         print(f"    number of contexts: {len(contexts)}")
         texts += contexts
 
@@ -231,59 +296,11 @@ def make_contexts(dataset, nb_files, pos_tags, window_size=1000):
     if nb_files is not None:
         nb_files = min(nb_files, len(files))
 
-    nb_docs = len(texts)
-    print(f"Total number of documents: {nb_docs}")
+    nb_contexts = len(texts)
+    print(f"Total number of contexts: {nb_contexts}")
     print(f"Total number of words: {nb_words}")
-    stats = dict(dataset=dataset, pos_set=sorted(pos_tags), nb_docs=nb_docs, nb_words=nb_words)
+    stats = dict(dataset=dataset, pos_set=sorted(pos_tags), nb_docs=nb_contexts, nb_words=nb_words)
     return texts, stats, nb_files
-
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--dataset', type=str, required=True)
-    parser.add_argument('--version', type=str, required=False, default='default')
-    parser.add_argument('--nb-files', type=int, required=False, default=None)
-    parser.add_argument('--window', type=int, required=False, default=1000)
-    parser.add_argument('--min-word-freq', type=int, required=False, default=50)
-    parser.add_argument('--min-contexts', type=int, required=False, default=40)
-    parser.add_argument('--nb-topics', type=int, required=False, default=300)
-    parser.add_argument('--pos-tags', nargs='*', type=str, required=False)
-
-    parser.add_argument('--tfidf', dest='tfidf', action='store_true', required=False)
-    parser.add_argument('--no-tfidf', dest='tfidf', action='store_false', required=False)
-    parser.set_defaults(tfidf=False)
-
-    parser.add_argument('--make-contexts', dest='make_contexts', action='store_true')
-    parser.add_argument('--load-contexts', dest='make_contexts', action='store_false')
-    parser.set_defaults(make_contexts=True)
-
-    parser.add_argument('--make-corpus', dest='make_corpus', action='store_true')
-    parser.add_argument('--load-corpus', dest='make_corpus', action='store_false')
-    parser.set_defaults(make_corpus=True)
-
-    parser.add_argument('--make-lsi', dest='make_lsi', action='store_true')
-    parser.add_argument('--load-lsi', dest='make_lsi', action='store_false')
-    parser.set_defaults(make_lsi=True)
-
-    args = parser.parse_args()
-
-    args.dataset = DATASETS.get(args.dataset, args.dataset)
-
-    if args.pos_tags is None:
-        if args.version == 'all':
-            args.pos_tags = []
-        elif args.version == 'noun':
-            args.pos_tags = POS_N
-        elif args.version == 'noun-verb':
-            args.pos_tags = POS_NV
-        elif args.version == 'noun-verb-adj':
-            args.pos_tags = POS_NVA
-        else:
-            args.pos_tags = POS_N
-    args.pos_tags = set(args.pos_tags)
-
-    return args
 
 
 def main():
@@ -293,7 +310,6 @@ def main():
     logger = init_logging(
         name=f"MM_{args.dataset}_{corpus_type}", basic=False, to_stdout=True, to_file=True
     )
-    logg = logger.info if logger else print
     log_args(logger, args)
 
     file_name = f'{args.dataset}_{args.version}'
@@ -304,21 +320,22 @@ def main():
     if args.make_contexts:
         # - make contexts -
         contexts, stats, nb_files = make_contexts(
-            args.dataset, args.nb_files, args.pos_tags, window_size=args.window
+            dataset=args.dataset,
+            min_freq=args.min_word_freq,
+            min_contexts=args.min_contexts,
+            nb_files=args.nb_files,
+            pos_tags=args.pos_tags,
+            window_size=args.window,
         )
-
-        if args.min_word_freq > 0:
-            # TODO: The approach for removal of infrequent words needs to be reconsidered.
-            #       It should ideally be applied after the dataset is initially read.
-            contexts = remove_infrequent_words(contexts, args.min_word_freq)
-
         gc.collect()
 
         # - save contexts -
-        file_path = directory / f'{file_name}_texts.json'
-        logg(f"Saving {file_path}")
+        file_path = directory / f'{file_name}_contexts.txt'
+        print(f"Saving {file_path}")
         with open(file_path, 'w') as fp:
-            json.dump(contexts, fp, ensure_ascii=False)
+            for context in contexts:
+                context = ' '.join(context).replace('\n', '<P>')
+                fp.write(context + '\n')
 
         # - save stats -
         file_path = directory / f'{file_name}_stats.json'
@@ -327,18 +344,16 @@ def main():
             json.dump(stats, fp)
     else:
         # - load contexts -
-        file_path = directory / f'{file_name}_contexts.json'
+        file_path = directory / f'{file_name}_contexts.txt'
         print(f"Loading {file_path}")
         with open(file_path, 'r') as fp:
-            contexts = json.load(fp)
+            contexts = [c.split() for c in fp.readlines()]
 
     # --- Make or Load a Matrix Market corpus which will be either entropy-normalized
     #     or tf-idf transformed ---
     if args.make_corpus:
         # - make bow corpus -
-        corpus, dictionary = texts2corpus(
-            contexts, stopwords=None, min_contexts=args.min_contexts, filter_above=1
-        )
+        corpus, dictionary = texts2corpus(contexts, stopwords=None)
 
         # - save dictionary -
         file_path = directory / f'{file_name}.dict'
